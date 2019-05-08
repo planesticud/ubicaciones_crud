63ea1403d6547e5039ac9b1ca570a7be75644ab4
miguerpe <miguepe@hotmail.com>
2019-05-08 12:16:55 -0500

    Actualización rutas planestic

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "github.com/udistrital/ubicaciones_crud/routers"
+	_ "github.com/planesticud/ubicaciones_crud/routers"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
